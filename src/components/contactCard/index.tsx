51fc52becdd31e6187c1f3301ac2287d015ab78b
Gabi Possiderio <gabipossiderio@gmail.com>
2024-01-02 17:22:08 -0300

    update resume and adjust contact card size

--- src/components/contactCard/index.tsx
+++ src/components/contactCard/index.tsx
@@ -7,11 +7,11 @@ interface CardProps {
 
 export function ContactCard({name, Icon, description, href}:CardProps) {
   return(
-  <a href={href} target="_blank"><article className=" h-56 duration-500 max-w-56 flex-col gap-2 menu w-56 rounded-lg flex md:mt-10 transition-all drop-shadow-md bg-white/60 hover:scale-105 text-stone-700 dark:text-white items-center justify-center dark:bg-amber-600/60">
-  <div className="dark:bg-stone-700 bg-amber-600/60 rounded-full p-3 drop-shadow-md text-5xl"><Icon /></div>
-   <h1 className="text-2xl flex pt-2 flex-col justify-center items-center">
+  <a href={href} target="_blank"><article className=" md:h-56 h-40 duration-500 max-w-56 flex-col gap-2 menu md:w-56 w-40 rounded-lg flex md:mt-10 transition-all drop-shadow-md bg-white/60 hover:scale-105 text-stone-700 dark:text-white items-center justify-center dark:bg-amber-600/60">
+  <div className="dark:bg-stone-700 bg-amber-600/60 rounded-full p-3 drop-shadow-md text-4xl"><Icon /></div>
+   <h1 className="md:text-2xl text-lg flex pt-2 flex-col justify-center items-center">
       {name} 
    </h1>
- <p className="text-md">{description}</p>
+ <p className="md:text-md text-xs">{description}</p>
  </article></a>)
 }
